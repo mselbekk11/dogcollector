cf0de866feb06372b714f3ea5ad1d344237e31c0
mselbekk11 <mselbekk11@gmail.com>
2023-12-10 19:59:54 -0800

    updates

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.urls import reverse
+
+MEALS = (
+  ('B', 'Breakfast'),
+  ('L', 'Lunch'),
+  ('D', 'Dinner')
+)
 
 # Create your models here.
+class Dog(models.Model):
+  name = models.CharField(max_length=100)
+  breed = models.CharField(max_length=100)
+  description = models.TextField(max_length=250)
+  age = models.IntegerField()
+
+  def __str__(self):
+    return f'{self.name} ({self.id})'
+  
+  def get_absolute_url(self):
+    return reverse('detail', kwargs={'dog_id': self.id})
+  
+
+class Feeding(models.Model):
+  date = models.DateField('feeding date')
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+  )
+  # Create a cat_id FK
+  dog = models.ForeignKey(
+    Dog,
+    on_delete=models.CASCADE
+  )
+
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
+  
+  class Meta:
+    ordering = ['-date']
